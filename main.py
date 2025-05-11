a75d35a0875402325125ec1a01565d2342707761
exoduxnile <sandeep.tete2012@gmail.com>
2025-05-11 15:57:34 +0530

    app = FastAPI()

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from fastapi.responses import HTMLResponse
 from fastapi.exception_handlers import http_exception_handler
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
+app = FastAPI()
+
+
 @app.exception_handler(StarletteHTTPException)
 async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
     if exc.status_code == 503:
